b5ac40e4318053892fcb1c388149a1f66af83cdd
unknown <acidddropdesign@gmail.com>
2022-06-28 16:41:53 +0200

    testing ...

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
-fetch('http://localhost:3000/api/products/')
-.then (res => res.json())
-.then (data => console.log(data))
-
+fetch("http://localhost:3000/api/products")
+.then (res => res.json()) 
+.then (data => console.log(data)) 
+    
+var element = document.getElementById("items");
+element.innerHTML = `<h3 class="productName">""</h3>` 
 
