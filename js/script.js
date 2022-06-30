9b2cb0bc179606a313646031aec20cfe5d443381
unknown <acidddropdesign@gmail.com>
2022-06-30 11:55:27 +0200

    Affichage produits index v1

--- js/script.js
+++ js/script.js
@@ -1,7 +1,28 @@
-fetch("http://localhost:3000/api/products")
-.then (res => res.json()) 
-.then (data => console.log(data)) 
-    
-var element = document.getElementById("items");
-element.innerHTML = `<h3 class="productName">""</h3>` 
+let products = [];
 
+const fetchProduct = async () => {
+    await fetch("http://localhost:3000/api/products")
+    .then((res) => res.json())
+    .then((promise) => { 
+        products = promise;
+        console.log(products);
+    })
+};
+
+const listProducts = async () => {
+    await fetchProduct();
+
+    document.getElementById("items").innerHTML += products.map(
+    (product) => ` <a href="${product._id}">
+    <article>
+      <img src="${product.imageUrl}" alt="${product.altTxt}">
+      <h3 class="productName">${product.name}</h3>
+      <p class="productDescription">${product.description}</p>
+    </article>
+  </a>
+    `
+
+    );
+};
+
+listProducts()
