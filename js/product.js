9ea37098ca63790b8ad59818c6cfcbf3fec7aa8e
unknown <acidddropdesign@gmail.com>
2022-07-04 17:17:17 +0200

    ajout titre,description,image

--- js/product.js
+++ js/product.js
@@ -2,6 +2,8 @@
 let str = window.location.href;
 let  url = new URL(str);
 let id = url.searchParams.get("id");
+let productInfo = [];
+
 console.log(id)
 
 
@@ -9,9 +11,21 @@ const fetchProduct = async () => {
     await fetch (`http://localhost:3000/api/products/${id}`)
     .then((res) => res.json())
     .then((promise) => { 
-        console.log(promise);
+        productInfo = promise;
+        console.log(productInfo);
     })
 };
 
-fetchProduct()
+const listProducts = async () => {
+    await fetchProduct();
 
+   document.getElementsByClassName("item__img")[0].innerHTML += ` <img src="${productInfo.imageUrl}" alt="${productInfo.altTxt}"> `
+   document.getElementById("title").innerHTML += ` ${productInfo.name}`
+   document.getElementById("description").innerHTML += ` ${productInfo.description} `
+   document.getElementById("colors").innerHTML += ` <option value="${productInfo.colors}">${productInfo.colors}</option> `
+    
+};
+
+
+
+listProducts()
