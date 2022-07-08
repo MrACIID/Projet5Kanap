35bf38c396348be78b5069fac28ed97853d5cb50
unknown <acidddropdesign@gmail.com>
2022-07-08 13:17:44 +0200

    ajout commentaire, stockage url api dans variable

--- js/product.js
+++ js/product.js
@@ -1,20 +1,25 @@
+// Declaration de l'URL et récupération de l'ID avec URL.SearchParams
 
 let str = window.location.href;
 let  url = new URL(str);
 let id = url.searchParams.get("id");
 let productInfo = [];
+let apiUrl = `http://localhost:3000/api/products/${id}`
 
 console.log(id)
 
+// Fetch de l'api sur l'id du produit et remplissage du tableau productInfo avec la promise
 
 const fetchProduct = async () => {
-    await fetch (`http://localhost:3000/api/products/${id}`)
+    await fetch (apiUrl)
     .then((res) => res.json())
     .then((promise) => { 
         productInfo = promise;
         console.log(productInfo);
     })
 };
+
+//Affichage du produit depuis l'HTML
 
 const listProducts = async () => {
     await fetchProduct();
