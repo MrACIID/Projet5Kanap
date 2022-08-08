b40f6df949f42aab8bc789710625296fa9eff900
unknown <acidddropdesign@gmail.com>
2022-08-08 16:17:28 +0200

    début bouton supprimer

--- js/cart.js
+++ js/cart.js
@@ -24,7 +24,7 @@ const showCart = async () => {
        
        document.getElementById("cart__items").innerHTML += 
 `
-<article class="cart__item" data-id="{product-ID}" data-color="{product-color}">
+<article class="cart__item" data-id="${idProd}" data-color="${colorProd}">
     <div class="cart__item__img">
         <img src="${productInfo.imageUrl}" alt="Photographie d'un canapé">
             </div>
@@ -36,20 +36,55 @@ const showCart = async () => {
                   </div>
                   <div class="cart__item__content__settings">
                     <div class="cart__item__content__settings__quantity">
-                      <p>Qté :${quantityProd} </p>
-                      <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="0">
+                      <p>Qté : </p>
+                      <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${quantityProd}">
                     </div>
                     <div class="cart__item__content__settings__delete">
                       <p class="deleteItem">Supprimer</p>
                     </div>
              </div>
      </div>
-</article>  `  
+</article>  ` 
+
+
+
+
+
+document.getElementById("totalQuantity").innerHTML += ``
+document.getElementById("totalPrice").innerHTML += ``
+
+
+
+
+////////////MISE EN PLACE BOUTON SUPRIMER
+
+let deleteBtn = document.querySelectorAll(".deleteItem");
+for (let l = 0; l < deleteBtn.length; l++){
+  deleteBtn[l].addEventListener("click", (event) => {
+    event.preventDefault();
+
+    //////Selection du produit a supprimer
+
+    let selectedId = localstorageProducts[l].id
+    localstorageProducts = localstorageProducts.filter( el => el.id !== selectedId)
+        
+
+   /////Changement du localstorage et reload de la page
+   localStorage.setItem("panier" , JSON.stringify(localstorageProducts));
+   window.location.href = "cart.html";
+
+  })
+
+
+}
+
     }
 
     showCart();
 
-}
+
+    
+};
 
 
 
